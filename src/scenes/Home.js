aa11fb179444fa3eb91392fd24a10eca997b63b1
xuyaozu <2589231996@qq.com>
2020-09-30 15:40:00 +0100

    Fix: Remove outer ScrollView

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
 import NavHeader from '@/components/common/NavHeader';
 import MainListView from '@/components/home/MainListView';
 
@@ -8,8 +7,8 @@ const toDoList = [
     planName: '今日安排',
     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
     taskAmount: 9,
+    toggle: false,
     taskList: [
-      { taskName: '刷牙', scheduledTime: '9:00', status: true },
       { taskName: '刷牙', scheduledTime: '9:00', status: true },
       { taskName: '刷牙', scheduledTime: '9:00', status: true },
       { taskName: '刷牙', scheduledTime: '9:00', status: true },
@@ -27,6 +26,7 @@ const toDoList = [
     planName: '今日安排',
     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
     taskAmount: 9,
+    toggle: false,
     taskList: [
       { taskName: '刷牙', scheduledTime: '9:00', status: true },
       { taskName: '刷牙', scheduledTime: '9:00', status: true },
@@ -47,9 +47,7 @@ const toDoList = [
 const App = () => (
   <>
     <NavHeader />
-    <ScrollView>
-      <MainListView toDoList={toDoList} />
-    </ScrollView>
+    <MainListView toDoList={toDoList} />
   </>
 );
 export default App;
