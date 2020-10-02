a3b08113e009ec4c04ae60ee396402660b584cdd
xuyaozu <2589231996@qq.com>
2020-10-02 09:53:25 +0100

    Fix: Beautify the ui of header

--- src/components/common/NavHeader.js
+++ src/components/common/NavHeader.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Header } from 'react-native-elements';
 import HeaderBackground from '@/assets/image/headerBackground.jpeg';
+import AddButton from './AddButton';
 
 const NavHeader = () => (
   <Header
     backgroundImage={HeaderBackground}
     placement="center"
-    leftComponent={{ icon: 'menu', color: '#fff' }}
-    centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-    rightComponent={{ icon: 'add', color: '#fff' }}
+    leftComponent={{
+      icon: 'menu',
+      color: '#fff',
+      style: { fontWeight: 'bold' },
+    }}
+    centerComponent={{
+      text: 'Trailing',
+      style: { color: '#fff', fontSize: 20, fontWeight: 'bold' },
+    }}
+    rightComponent={() => <AddButton />}
   />
 );
 
