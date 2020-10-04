58846fc3ac4e2aa532e446b04621537caa774145
xuyaozu <2589231996@qq.com>
2020-10-04 20:41:07 +0100

    Feature: Add toggle function

--- src/components/home/MainListView.js
+++ src/components/home/MainListView.js
@@ -1,22 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import {
   ListItem, Avatar,
 } from 'react-native-elements';
 import SwipeList from '@/components/home/SwipeList';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  subTitle: {
+    paddingTop: 4,
+  },
+});
 
 const MainListView = (props) => {
-  const [currentPlanIndex, setcurrentPlanIndex] = useState(false);
   const { toDoList } = props;
+  const dispatch = useDispatch();
   return (
     toDoList.map((item, index) => (
       <>
         <ListItem
           bottomDivider
         >
-          <Avatar source={{ uri: item.avatar_url }} />
+          <Avatar
+            rounded
+            source={{ uri: item.avatar_url }}
+            size="medium"
+          />
           <ListItem.Content>
             <ListItem.Title>{item.planName}</ListItem.Title>
-            <ListItem.Subtitle>
+            <ListItem.Subtitle style={styles.subTitle}>
               任务数:
               {' '}
               {item.taskAmount}
@@ -27,7 +39,7 @@ const MainListView = (props) => {
             name="angle-down"
             type="font-awesome"
             onPress={() => {
-              setcurrentPlanIndex(index);
+              dispatch({ type: 'SET_TOOGLE' });
             }}
           />
         </ListItem>
