78f58c14fa8f42d564e5e7739dfa84804f156cd7
xuyaozu <2589231996@qq.com>
2020-10-09 10:36:42 +0100

    Feature: Add common style shadow

--- src/assets/style/base.js
+++ src/assets/style/base.js
@@ -4,6 +4,16 @@ const baseStyle = StyleSheet.create({
   bottomTabs: {
     fontSize: 18,
   },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
 });
 
 export default baseStyle;
