8ab1a9d45db0da9a179499a9cf093f0cc8d1ea86
Yaozu-Xu <2589231996@qq.com>
2020-10-13 10:10:53 +0100

    Feature: Add new tab for login navigation

---
+++ src/components/tabs/LoginTab.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import BaseStyle from '@/assets/style/base';
+
+const LoginTab = () => (
+  <Icon name="user-o" style={BaseStyle.bottomTabs} />
+);
+
+export default LoginTab;
