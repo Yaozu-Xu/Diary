93d36021f25d4e81b4558b1a1ce05b01a99d470e
Yaozu-Xu <2589231996@qq.com>
2020-10-15 10:42:23 +0100

    Featrue: Add component Initialization for init planLists

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import BottomNavigator from '@/navigations/BottomNavigator';
-import { navigationRef } from '@/navigations/RootNavigator';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '@/store';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomNavigator from '@/navigations/BottomNavigator';
+import { navigationRef } from '@/navigations/RootNavigator';
 import Auth from '@/components/common/Auth';
+import Initialization from '@/components/common/Initialization';
 
 const store = createStore(reducers);
 
 const App = () => (
   <Provider store={store}>
     <Auth />
+    <Initialization />
     <NavigationContainer ref={navigationRef}>
       <BottomNavigator />
     </NavigationContainer>
---
+++ src/components/common/Initialization.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import plansCollection from '@/requester/Plans';
+
+const Initialization = () => {
+  const dispatch = useDispatch();
+  const uid = '46JDW7egRJOUycYK5HYpfvCdy3j2';
+
+  function setPlanInStore(documentSnapshot) {
+    const payload = documentSnapshot.data();
+    dispatch({ type: 'SET_PLANS', payload });
+  }
+  useEffect(() => {
+    const subscriber = plansCollection.plansDocumentSnapshot(uid, setPlanInStore);
+    return subscriber;
+  }, [uid]);
+  return (
+    <></>
+  );
+};
+
+export default Initialization;
