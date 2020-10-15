b0ed08729102c1a549b22437cdbb871b9dccf05a
Yaozu-Xu <2589231996@qq.com>
2020-10-15 10:41:02 +0100

    Feature: combine the reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import displayReducer from './reducer/DisplayReducer';
 import userReducdr from './reducer/UserReducer';
+import plansReducdr from './reducer/PlansReducer';
+import tasksReducdr from './reducer/TasksReducer';
 
 export default combineReducers({
   display: displayReducer,
   user: userReducdr,
+  plans: plansReducdr,
+  task: tasksReducdr,
 });
