8c71c8cb678972756aadacb3f80f73e5abeec09d
Yaozu-Xu <2589231996@qq.com>
2020-10-19 10:48:18 +0100

    Refactor: redesign the logic of setting data and props

--- src/screens/Plans.js
+++ src/screens/Plans.js
@@ -1,24 +1,31 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
 import NavHeader from '@/components/headers/NavHeader';
-import MainListView from '@/components/ListViews/MainListView';
+import PlansHeader from '@/components/headers/PlansHeader';
 import SwipeList from '@/components/ListViews/SwipeList';
 import SelectionOverlay from '@/components/Overlays/SelectionOverlay';
+import NewPlanOverlay from '@/components/Overlays/NewPlanOverlay';
 import plansCollection from '@/requester/Plans';
+import { generatePid } from '@/utils/timer';
 
-const PlansScreen = () => {
-  const uid = '46JDW7egRJOUycYK5HYpfvCdy3j2';
-  const pid = 'wj1gooYRtymWkb65NFP7';
-
+const PlansScreen = ({ route }) => {
+  // date: '2020-8-13 format
+  const { date } = route.params;
   const dispatch = useDispatch();
-  const plan = useSelector((state) => state.plans.details.filter(
-    (item) => item.pid === pid,
-  ));
+  // const pid = generatePid(date);
+  const pid = 'wj1gooYRtymWkb65NFP7';
+  const uid = '46JDW7egRJOUycYK5HYpfvCdy3j2';
+
   function setTasksInStore(documentSnapshot) {
-    const payload = documentSnapshot.data();
-    payload.pid = pid;
-    dispatch({ type: 'SET_TASKS', payload });
+    const taskRes = documentSnapshot.data();
+    // 今天有计划
+    if (taskRes) {
+      taskRes.pid = pid;
+      dispatch({ type: 'SET_TASKS', payload: taskRes });
+    }
   }
+
   useEffect(() => {
     const subscriber = plansCollection.tasksDocumentSnapshot(uid, pid, setTasksInStore);
     return subscriber;
@@ -27,10 +34,27 @@ const PlansScreen = () => {
     <>
       <NavHeader />
       <SelectionOverlay />
-      <MainListView plan={plan} />
+      <NewPlanOverlay />
+      <PlansHeader date={date} />
       <SwipeList />
     </>
   );
+};
+
+PlansScreen.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      date: PropTypes.string,
+    }),
+  }),
+};
+
+PlansScreen.defaultProps = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      date: new Date(),
+    }),
+  }),
 };
 
 export default PlansScreen;
