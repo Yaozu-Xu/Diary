2471e29807f41474e2c8a6a686b679a732f483e3
Yaozu-Xu <2589231996@qq.com>
2020-10-23 11:51:59 +0100

    Fix: redesign the generatePid function

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-bitwise */
+import md5 from 'md5';
+
 /**
  *
  * @param {*} seconds current seconds
@@ -27,16 +28,7 @@ export const getCurrentHour = (date) => {
   return { minutes, hours };
 };
 
-export const generatePid = (str, seed = 0) => {
-  let h1 = 0xdeadbeef ^ seed; let
-    h2 = 0x41c6ce57 ^ seed;
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0, ch; i < str.length; i++) {
-    ch = str.charCodeAt(i);
-    h1 = Math.imul(h1 ^ ch, 2654435761);
-    h2 = Math.imul(h2 ^ ch, 1597334677);
-  }
-  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+export const generatePid = (msg) => {
+  const salt = 'asdaklsdasdjk';
+  return md5(msg + salt).substring(4);
 };
