2df6aa238e2ba0a5c7cde084c1ffbeb3ad49102e
Yaozu-Xu <2589231996@qq.com>
2020-10-23 11:54:30 +0100

    Feature: Add new requester functions for tasks and plans

--- src/requester/Plans.js
+++ src/requester/Plans.js
@@ -5,7 +5,7 @@ class PlansCollection {
     this.plansCollection = firestore().collection('Plans');
   }
 
-  // Getter functions
+  // Get all plans
   plansDocumentSnapshot(uid, func) {
     this.plansCollection.doc(uid)
       .onSnapshot((documentSnapshot) => {
@@ -13,10 +13,62 @@ class PlansCollection {
       });
   }
 
+  plansDocumentSetter(uid, { pid, date }) {
+    this.plansCollection.doc(uid).update(
+      {
+        details: firestore.FieldValue.arrayUnion(
+          { pid, date: firestore.Timestamp.fromDate(new Date(date)) },
+        ),
+      },
+    );
+  }
+
   tasksDocumentSnapshot(uid, pid, func) {
     this.plansCollection.doc(uid)
       .collection('Tasks').doc(pid).onSnapshot((documentSnapshot) => {
         func(documentSnapshot);
+      });
+  }
+
+  tasksDocumentCreator(uid, pid, data, callback) {
+    this.plansCollection.doc(uid)
+      .collection('Tasks').doc(pid).set(
+        {
+          details: firestore.FieldValue.arrayUnion(
+            { ...data, date: firestore.Timestamp.fromDate(new Date(data.date)) },
+          ),
+        },
+      )
+      .then(() => {
+        callback();
+      });
+  }
+
+  tasksDocumentSetter(uid, pid, data, callback) {
+    this.plansCollection.doc(uid)
+      .collection('Tasks').doc(pid).update(
+        {
+          details: firestore.FieldValue.arrayUnion(
+            { ...data, date: firestore.Timestamp.fromDate(new Date(data.date)) },
+          ),
+        },
+      )
+      .then(() => {
+        callback();
+      });
+  }
+
+  tasksDocumentRemover(uid, pid, data, callback) {
+    this.plansCollection.doc(uid)
+      .collection('Tasks').doc(pid).update(
+        {
+          details: firestore.FieldValue.arrayRemove(
+            data,
+          ),
+        },
+      )
+      .then(() => {
+        callback();
       });
   }
 }
