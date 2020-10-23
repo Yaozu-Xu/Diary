057f7f70ab11050b24d68bfb3ee1914f9fdc2d16
Yaozu-Xu <2589231996@qq.com>
2020-10-23 11:53:36 +0100

    Feature: Finish the component NewPlanOverlay

--- src/components/Overlays/NewPlanOverlay.js
+++ src/components/Overlays/NewPlanOverlay.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Dimensions } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import { Overlay, Input } from 'react-native-elements';
+import { Overlay, Input, Button } from 'react-native-elements';
+import PropTypes from 'prop-types';
+import TimeSelector from '@/components/common/TimeSelector';
+import planCollection from '@/requester/Plans';
 
 const { width } = Dimensions.get('window');
 
@@ -20,24 +23,91 @@ const styles = StyleSheet.create({
   inputText: {
     marginLeft: 10,
   },
+  button: {
+    marginTop: 15,
+    marginRight: 20,
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
 });
 
-const NewPlanOverlay = () => {
+const NewPlanOverlay = (props) => {
+  const { details } = props;
+  const { date, pid } = details;
+  const [task, setTask] = useState();
   const dispatch = useDispatch();
+  const plans = useSelector((state) => state.plans.details);
   const visible = useSelector((state) => state.display.newPlanToggle);
+  const hour = useSelector((state) => state.time.hour);
+  const minute = useSelector((state) => state.time.minute);
+
+  function noPlanToday() {
+    return plans.filter((plan) => plan.pid === pid).length === 0;
+  }
+
+  function successCallBack() {
+    alert('success');
+  }
+
+  function onSubmit() {
+    const uid = '46JDW7egRJOUycYK5HYpfvCdy3j2';
+    // const pid = 'wj1gooYRtymWkb65NFP7';
+    const dateFormat = `${date} ${hour}:${minute}`;
+    const data = {
+      alarm: false, status: false, date: dateFormat, name: task,
+    };
+    if (noPlanToday(pid)) {
+      planCollection.plansDocumentSetter(uid, { pid, date });
+      planCollection.tasksDocumentCreator(uid, pid, data, successCallBack);
+    } else {
+      planCollection.tasksDocumentSetter(uid, pid, data, successCallBack);
+    }
+  }
+
+  function onBackdropPress() {
+    dispatch({ type: 'PLAN_TOOGLE' });
+    dispatch({ type: 'SET_MINUTE', payload: null });
+    dispatch({ type: 'SET_HOUR', payload: null });
+    setTask(null);
+  }
+
   return (
     <Overlay
       isVisible={visible}
-      onBackdropPress={() => dispatch({ type: 'PLAN_TOOGLE' })}
+      onBackdropPress={() => onBackdropPress()}
     >
-      <Input
-        placeholder="Task name"
-        inputStyle={styles.inputText}
-        containerStyle={styles.inputWrapper}
-        inputContainerStyle={styles.inputContainer}
-      />
+      <>
+        <Input
+          placeholder="Task name"
+          inputStyle={styles.inputText}
+          containerStyle={styles.inputWrapper}
+          inputContainerStyle={styles.inputContainer}
+          onChangeText={(v) => setTask(v)}
+        />
+        <TimeSelector date={date} />
+        <Button
+          containerStyle={styles.button}
+          title="Submit"
+          type="clear"
+          onPress={() => onSubmit()}
+        />
+      </>
     </Overlay>
   );
+};
+
+NewPlanOverlay.propTypes = {
+  details: PropTypes.shape({
+    date: PropTypes.string,
+    pid: PropTypes.string,
+  }),
+};
+
+NewPlanOverlay.defaultProps = {
+  details: PropTypes.shape({
+    date: '',
+    pid: '',
+  }),
 };
 
 export default NewPlanOverlay;
