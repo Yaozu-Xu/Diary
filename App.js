700f46fd94e43286d1a53e3bb6a9bf50d9a9fc66
Yaozu-Xu <2589231996@qq.com>
2020-10-24 21:37:50 +0100

    Fix: Remove Initialzation component into Plan Screen

--- App.js
+++ App.js
@@ -3,19 +3,17 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '@/store';
 import { NavigationContainer } from '@react-navigation/native';
-import BottomNavigator from '@/navigations/BottomNavigator';
+import StackNavigator from '@/navigations/StackNavigator';
 import { navigationRef } from '@/navigations/RootNavigator';
 import Auth from '@/components/common/Auth';
-import Initialization from '@/components/common/Initialization';
 
 const store = createStore(reducers);
 
 const App = () => (
   <Provider store={store}>
     <Auth />
-    <Initialization />
     <NavigationContainer ref={navigationRef}>
-      <BottomNavigator />
+      <StackNavigator />
     </NavigationContainer>
   </Provider>
 );
