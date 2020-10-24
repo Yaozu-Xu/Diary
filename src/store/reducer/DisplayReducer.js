358d69dc9050a7d0476f74b42b5ebbca2298cc1f
Yaozu-Xu <2589231996@qq.com>
2020-10-24 21:34:18 +0100

    Feature: Add toggle state for plan overlay

--- src/store/reducer/DisplayReducer.js
+++ src/store/reducer/DisplayReducer.js
@@ -2,12 +2,17 @@ const STATE = {
   // 0 = 全部 1 = 未完成 2 = 已完成
   currentSelection: 0,
   toggle: false,
+  newPlanToggle: false,
 };
 
 const displayReducer = (state = STATE, action) => {
   switch (action.type) {
     case 'SET_TOOGLE': {
       const newState = { ...state, toggle: !state.toggle };
+      return newState;
+    }
+    case 'PLAN_TOOGLE': {
+      const newState = { ...state, newPlanToggle: !state.newPlanToggle };
       return newState;
     }
     case 'SET_SELECTION': {
