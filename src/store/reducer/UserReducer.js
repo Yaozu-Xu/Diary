305ebe31e5eb21520b7b706ebc6e46a9b8eb5d6a
Yaozu-Xu <2589231996@qq.com>
2020-10-24 21:38:16 +0100

    Refactor: Redesign the set state

--- src/store/reducer/UserReducer.js
+++ src/store/reducer/UserReducer.js
@@ -8,7 +8,7 @@ const STATE = {
 const displayReducer = (state = STATE, action) => {
   switch (action.type) {
     case 'SET_USER': {
-      const newState = { ...state, ...action.payload };
+      const newState = action.payload;
       return newState;
     }
     default:
