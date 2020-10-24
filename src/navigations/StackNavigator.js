834559b70ec21b8392a9cd44af97e762f0d4332d
Yaozu-Xu <2589231996@qq.com>
2020-10-24 21:28:49 +0100

    Fix: Make the StackNavigor to become the outer navigator

--- src/navigations/StackNavigator.js
+++ src/navigations/StackNavigator.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '@/screens/User/Login';
-import IndexScreen from '@/screens/User/Index';
 import SignInScreen from '@/screens/User/SignIn';
+import IndexNavigator from '@/navigations/IndexNavigator';
 
 const Stack = createStackNavigator();
 
-const UserNavigator = () => (
+const StackNavigator = () => (
   <Stack.Navigator>
     <Stack.Screen
-      name="User"
-      component={IndexScreen}
+      name="LogIn"
+      component={LoginScreen}
     />
     <Stack.Screen
       name="SignIn"
       component={SignInScreen}
     />
     <Stack.Screen
-      name="LogIn"
-      component={LoginScreen}
+      options={{ headerShown: false }}
+      name="Index"
+      component={IndexNavigator}
     />
   </Stack.Navigator>
 );
 
-export default UserNavigator;
+export default StackNavigator;
